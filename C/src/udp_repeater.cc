1f9bc8fb20a7a6dd27809fa06cdf7c76153168af
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2021-07-20 15:56:04 -0500

    Initial commit

---
+++ C/src/udp_repeater.cc
@@ -0,0 +1,30 @@
+/**
+ * @file udp_repeater.cc
+ * @brief Main file for the udp repeater
+ */
+#include <gflags/gflags.h>
+#include <string>
+
+#include "udp_client.h"
+#include "udp_server.h"
+
+DEFINE_uint64(rx_port_id, 8080, "");
+DEFINE_uint64(tx_port_id, 8081, "");
+
+
+int main(int argc, char* argv[]) {
+  int ret = EXIT_FAILURE;
+
+  gflags::SetUsageMessage("rx_port_id, tx_port_id");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  //std::string cur_directory = TOSTRING(PROJECT_DIRECTORY);
+  
+  unsigned tx_port = FLAGS_tx_port_id;
+  unsigned rx_port = FLAGS_rx_port_id;
+  
+  std::printf("Tx port: %d, rx port :%d\n", tx_port, rx_port);
+  ret = EXIT_SUCCESS;
+  std::printf("Udp Repeater application terminated!\n");
+  gflags::ShutDownCommandLineFlags();
+  return ret;
+}
