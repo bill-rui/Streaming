6ffb87b1b56f7a33f8033e2161678dccbc601623
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-07-26 13:26:49 -0500

    .

---
+++ C/src/server_testing.cpp
@@ -0,0 +1,14 @@
+#include "udp_server.h"
+#include <iostream>
+#define BUFF_SIZE 2000
+
+int main(int argc, char const *argv[]){
+    unsigned char buffer[BUFF_SIZE];
+    UDPServer server(8080, 2000);
+    while (true){
+        server.Recv((unsigned char *) &buffer, BUFF_SIZE);
+        std::cout << buffer << std::endl;
+    }
+
+
+}
