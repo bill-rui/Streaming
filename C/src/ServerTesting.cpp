9ace1319064f4160ac22822604daa07de831a60a
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-07-27 14:47:55 -0500

    Server working for pure audio

--- C/src/ServerTesting.cpp
+++ C/src/ServerTesting.cpp
@@ -16,9 +16,18 @@ int main(int argc, char const *argv[]){
     UDPClient sender;
     server.MakeBlocking();
 
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    ssize_t s = 0;
+    int i = 0;
     while (true){
         ssize_t packetSize = server.Recv((unsigned char *) &buffer, BUFF_SIZE);
         sender.Send(FORWARDING_ADDR, FORWARDING_PORT, (unsigned char *) &buffer, packetSize);
+        s += packetSize;
+        if(std::chrono::steady_clock::now() - start > std::chrono::seconds(i)){
+            i++;
+            std::cout << s << std::endl;
+            s = 0;
+        }
     }
 }
 #pragma clang diagnostic pop
