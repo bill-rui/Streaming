8ad8811e5fb9b77b02608228741ed14528ebc66f
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-07-28 14:31:21 -0500

    Working with consistent packet sizes (larger or smaller)

--- C/src/ServerTesting.cpp
+++ C/src/ServerTesting.cpp
@@ -7,9 +7,9 @@
 #pragma ide diagnostic ignored "EndlessLoop"
 #define BUFF_SIZE 4000
 #define SERVER_PORT 1350
-#define FORWARDING_ADDR "10.238.200.105"
+#define FORWARDING_ADDR "10.238.200.107"
 #define FORWARDING_PORT 1234
-#define SEND_PACKET_SIZE 512
+#define SEND_PACKET_SIZE 2632
 
 int main(int argc, char const *argv[]){
     unsigned char rxBuffer[BUFF_SIZE + SEND_PACKET_SIZE];
@@ -19,9 +19,10 @@ int main(int argc, char const *argv[]){
     server.MakeBlocking();
     unsigned long bufferSize = 0;
     unsigned long sendPacketSize;
+    auto *recvStart = (unsigned char *) &rxBuffer;
 
     while(true){
-        ssize_t packetSize = server.Recv((unsigned char *) &rxBuffer, BUFF_SIZE);
+        ssize_t packetSize = server.Recv(recvStart, BUFF_SIZE);
         std::cout << "packet received: " << packetSize << std::endl;
         bufferSize += packetSize;
         auto *bufferPtr = (unsigned char *) &rxBuffer;
@@ -30,7 +31,7 @@ int main(int argc, char const *argv[]){
 
         if(SEND_PACKET_SIZE > bufferSize){
             sendPacketSize = bufferSize;
-            packetCount = 1;
+            packetCount = 0;
             leftoverBytes = 0;
         }
         else{
@@ -41,8 +42,8 @@ int main(int argc, char const *argv[]){
 
         for(unsigned int i = 0; i < packetCount; i++){  // send packets
             try{
-                sender.Send(FORWARDING_ADDR, FORWARDING_PORT, bufferPtr, sendPacketSize);
-                std::cout << "packet sent: " << sendPacketSize << std::endl;
+                ssize_t s = sender.Send(FORWARDING_ADDR, FORWARDING_PORT, bufferPtr, sendPacketSize);
+                std::cout << "packet sent: " << s << std::endl;
             }
             catch (std::runtime_error& e) {
                 std::cout << "Sending error: " << e.what() << std::endl;
@@ -50,8 +51,9 @@ int main(int argc, char const *argv[]){
             bufferPtr += sendPacketSize;
         }
 
-        memcpy(&rxBuffer, bufferPtr, leftoverBytes);
+        memcpy(&rxBuffer, bufferPtr, leftoverBytes);  //move buffer to front
         bufferSize -= packetCount * sendPacketSize;
+        recvStart = (unsigned char *) &rxBuffer + leftoverBytes;  // move the receiving function write position
     }
 }
 #pragma clang diagnostic pop
